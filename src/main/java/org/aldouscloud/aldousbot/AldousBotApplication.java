9f09a882b574dd26f50e380449502d9a68884b8f
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-07-21 18:39:28 +0500

    1 test the work TelegramBot and Core

---
+++ src/main/java/org/aldouscloud/aldousbot/AldousBotApplication.java
@@ -0,0 +1,13 @@
+package org.aldouscloud.aldousbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldousBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AldousBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aldouscloud/aldousbot/AldousBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aldouscloud.aldousbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldousBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
